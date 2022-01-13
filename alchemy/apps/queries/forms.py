47b9fa36f2787fa0bccefba3707e1d6088a10c6a
CTS-crypto <72420685+CTS-crypto@users.noreply.github.com>
2022-01-13 01:54:20 -0500

    queries

--- alchemy/apps/queries/forms.py
+++ alchemy/apps/queries/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from django.forms import Form
+from apps.subjects.models import Subject
+
+_subjects=Subject.objects.all()
+subjects=[(s.name,s.name) for s in _subjects]
 
 #Mostrar los n estudiantes con mas creditos acumulados (ordena descendiente).
 class MoreCreditsForm(Form):
@@ -12,17 +16,17 @@ class MoreElementsCreatedForm(Form):
 #Mostrar los n elementos mas utilizados por los estudiantes para realizar mezclas.
 class MoreBasicElementsUsedForm(Form):
     n_elements_basic = forms.IntegerField(min_value=1, required=True, initial=10)
-    subject_name = forms.CharField(label="Nombre de la asignatura", required=True)
+    subject_name = forms.ChoiceField(choices=subjects,label="Nombre de la asignatura", required=True)
 
 #Mostrar los n elmentos mas valiosos de los elementos no basicos.
 class MoreValuableNoBasicElementsForm(Form):
     n_elements_noBasic = forms.IntegerField(min_value=1, required=True, initial=10)
-    subject_name = forms.CharField(label="Nombre de la asignatura", required=True)
+    subject_name = forms.ChoiceField(choices=subjects,label="Nombre de la asignatura", required=True)
 
 #Mostrar los n elementos mas valiosos de los elementos basicos.
 class MoreValuableBasicElementsForm(Form):
     n_elements_valbasic = forms.IntegerField(min_value=1, required=True, initial=10)
-    subject_name = forms.CharField(label="Nombre de la asignatura", required=True)
+    subject_name = forms.ChoiceField(choices=subjects,label="Nombre de la asignatura", required=True)
 
 #Dado un mes, un año o un dia: mostrar los elementos creados
 class ElementsCreatedByDayForm(Form):
@@ -41,12 +45,12 @@ class ElementsCreatedDates:
 
 #Dado una asignatura: mostrar el ranking de estudiantes
 class SubjectStudentsCreditsForm(Form):
-    subject_name = forms.CharField(label="Nombre de la asignatura", required=True)
+    subject_name = forms.ChoiceField(choices=subjects,label="Nombre de la asignatura", required=True)
 
 #Dado una asignatura: mostrar los n estudiantes con mas creditos
 class SubjectStudentsCreditsNForm(Form):
     n_students_subCredits = forms.IntegerField(min_value=1, required=True, initial=10)
-    subject_name = forms.CharField(label="Nombre de la asignatura", required=True)
+    subject_name = forms.ChoiceField(choices=subjects,label="Nombre de la asignatura", required=True)
 
 #Dado un intervalo de fechas mostrar el ranking de los estudiantes (por creditos totales)
 class RankingByDatesTotalCreditsForm(Form):
@@ -57,4 +61,4 @@ class RankingByDatesTotalCreditsForm(Form):
 class RankingByDatesSubjectCredits(Form):
     initial_date = forms.DateField(required=True)
     final_date = forms.DateField(required=True)
-    subject_name = forms.CharField(label="Nombre de la asignatura", required=True)
+    subject_name = forms.ChoiceField(choices=subjects,label="Nombre de la asignatura", required=True)
